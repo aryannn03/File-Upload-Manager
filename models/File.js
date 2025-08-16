042a6bf5ed63fae6b813a2511b8b2a06a54828c9
aryan <124302386+aryannn03@users.noreply.github.com>
2025-08-17 02:26:55 +0530

    node mailer added

--- models/File.js
+++ models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const nodemailer = require("nodemailer")
+
 
 const fileSchema = new mongoose.Schema({
   name: {
@@ -14,6 +16,27 @@ const fileSchema = new mongoose.Schema({
   email:{
     type: String,
   }
+});
+
+fileSchema.post("save",async function(doc){
+   try{
+    let transporter = nodemailer.createTransport({
+      host:process.env.MAIL_HOST,
+      auth:{
+        user:process.env.MAIL_USER,
+        pass:process.env.MAIL_PASS,
+      }
+    });
+    let info = await transporter.sendMail({
+      from:"Aryan",
+      to:doc.email,
+      subject: "New file Uploaded on Cloudinary",
+      html:`<h2>Hello Jee</h2> <p>File Uploaded </p>`
+    })
+   }
+   catch(error){
+    console.error(error);
+   }
 })
 const File = mongoose.model('File', fileSchema);
 module.exports = File;
